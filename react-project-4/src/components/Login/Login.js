8e3cd57d775ed43a4b219a9bfe38766db045cdd4
Shresthikashyap <sk2929542@gmail.com>
2023-11-25 22:09:32 +0530

    custom context provider component

--- react-project-4/src/components/Login/Login.js
+++ react-project-4/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState,useReducer, useEffect } from 'react';
+import React, { useState,useReducer, useEffect, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state,action) => {
   if(action.type === 'USER_INPUT'){
@@ -25,10 +26,9 @@ const passwordReducer = (state,action) => {
 };
 
 const Login = (props) => {
-  // const [enteredEmail, setEnteredEmail] = useState('');
-  // const [emailIsValid, setEmailIsValid] = useState();
-  // const [enteredPassword, setEnteredPassword] = useState('');
-  // const [passwordIsValid, setPasswordIsValid] = useState();
+  
+  const authctx = useContext(AuthContext);
+
   const [enteredCollege, setEnteredCollege] = useState('');  
   const [collegeIsValid, setCollegeIsValid] = useState(''); 
   const [formIsValid, setFormIsValid] = useState(false);
@@ -89,7 +89,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value, enteredCollege);
+    authctx.onLogin(emailState.value, passwordState.value, enteredCollege);
   };
 
   return (
