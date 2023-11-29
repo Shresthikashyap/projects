f68370bc4cbf05c4fdf1c82aa74ab9e95ed35771
Shresthikashyap <sk2929542@gmail.com>
2023-11-29 14:13:34 +0530

    Add meals button

--- Restaurant App/src/components/Meals/AvailableMeals.js
+++ Restaurant App/src/components/Meals/AvailableMeals.js
@@ -12,33 +12,33 @@ const DUMMY_MEALS = [
         price:'₹100'
     },
     {
-        id:'m1',
-        name:'Butter Chicken',
-        description:'dumbhead it does not need any description',
-        price:'₹100'
+        id:'m2',
+        name:'Pasta',
+        description:'delicioso',
+        price:'₹80'
     },
     {
-        id:'m1',
-        name:'Butter Chicken',
-        description:'dumbhead it does not need any description',
-        price:'₹100'
+        id:'m3',
+        name:'Pizza',
+        description:'Its tasty but you cannot live on it. Learn how to Cook Roti and Sabji',
+        price:'₹110'
     },
     {
-        id:'m1',
-        name:'Butter Chicken',
-        description:'dumbhead it does not need any description',
+        id:'m4',
+        name:'Peeli wali dal with roti',
+        description:'Till you are learning how to cook, let us serve you with ghar ka khaana',
         price:'₹100'
     }
 ]
 
-const AvailableMeals = (props) =>{
+const AvailableMeals = () =>{
 
     const availableMeals = DUMMY_MEALS.map((meals) =>
-    <MealItem name={meals.name} description = {meals.description} price = {meals.price}/>)
+    <MealItem key= {meals.id} name={meals.name} description = {meals.description} price = {meals.price}/>)
     return (
         <section className={classes.meals}>
             <Card>
-               {availableMeals}
+               <ul>{availableMeals}</ul>
             </Card>
         </section>
     )
