3ec5b7ee02dab236d38c889661b8dd7fea160912
Shresthikashyap <122147931+Shresthikashyap@users.noreply.github.com>
2024-02-03 12:15:43 +0530

    Update Counter.js
    Signed-off-by: Shresthikashyap <122147931+Shresthikashyap@users.noreply.github.com>

--- Redux-Counter/src/components/Counter.js
+++ Redux-Counter/src/components/Counter.js
@@ -1,24 +1,27 @@
 import { useDispatch, useSelector} from 'react-redux';
+import { counterActions } from './store';
 import classes from './Counter.module.css';
 
 
 const Counter = () => {
   
-  const counter = useSelector(state => state.counter)
+  const counter = useSelector(state => state.counter.counter);
+  const showCounter = useSelector(state => state.counter.showCounter);
   const dispatch = useDispatch();
 
-  const toggleCounterHandler = () => {};
+  //const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      { showCounter && <div className={classes.value}>{counter}</div>}
       <div>
-        <button onClick={(e)=>{dispatch({type:'INCREMENT'})}}>Increment</button>
-        <button onClick={(e)=>{dispatch({type:'DECREMENT'})}}>Decrement</button>      
+        <button onClick={(e)=>{dispatch(counterActions.increment())}}>Increment</button>
+        <button onClick={(e)=>{dispatch(counterActions.increase(2))}}>IncrementBy2</button>
+        <button onClick={(e)=>{dispatch(counterActions.decrement())}}>Decrement</button>      
       </div>
 
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <button onClick={() => dispatch(counterActions.toggleCounter())}>Toggle Counter</button>
     </main>
   );
 };
