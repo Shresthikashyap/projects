64319d3683bc0924902bcbd430fe0f937082aee0
Shresthikashyap <122147931+Shresthikashyap@users.noreply.github.com>
2024-02-07 14:43:46 +0530

    Add files via upload
    Signed-off-by: Shresthikashyap <122147931+Shresthikashyap@users.noreply.github.com>

--- ReduxCart/src/components/Shop/Products.js
+++ ReduxCart/src/components/Shop/Products.js
@@ -1,16 +1,41 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const products = [{
+  id: 1,
+  title: "Product 1",
+  price: 10.99,
+  description: "This is the description for Product 1."
+},
+{
+  id: 2,
+  title: "Product 2",
+  price: 19.99,
+  description: "This is the description for Product 2."
+},
+{
+  id: 3,
+  title: "Product 3",
+  price: 7.99,
+  description: "This is the description for Product 3."
+}
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {products.map((product)=>{
+          return <ProductItem
+            key={product.id}
+            id= {product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />        
+        })    
+        }
       </ul>
     </section>
   );
