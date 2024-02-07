cdbd55d5867bacaa58ad7f6761c3a7fa53c48d71
Shresthikashyap <122147931+Shresthikashyap@users.noreply.github.com>
2024-02-08 01:01:27 +0530

    Update App.js
    Signed-off-by: Shresthikashyap <122147931+Shresthikashyap@users.noreply.github.com>

--- ReduxCart/src/App.js
+++ ReduxCart/src/App.js
@@ -2,27 +2,29 @@ import { useDispatch, useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import Notification from './components/UI/Notification'
-import { Fragment, useEffect } from 'react';
-import { uiActions } from './components/store/ui-slice';
+import Notification from './components/UI/Notification';
+import { Fragment, useEffect, useState } from 'react';
+import { cartActions } from './components/store/cart-slice';
+import { uiActions } from './components/store/ui-slice'; // Assuming you have this file
 
-let isInitial = true; // This is the global variable
-
+//let isInitial = false;
 function App() {
   const dispatch = useDispatch();
   const showCart = useSelector(state => state.ui.cartIsVisible);
   const cart = useSelector(state => state.cart);
   const notification = useSelector(state => state.ui.notification);
+  const [requestInProgress, setRequestInProgress] = useState(false);
 
   useEffect(() => {
     const sendCartData = async () => {
       try {
-        dispatch(uiActions.showNotification({
+        setRequestInProgress(true);
+    
+           dispatch(uiActions.showNotification({
           status: "pending",
           title: "Sending",
           message: "Sending cart Data"
-        }));
-
+        }));  
 
 
         const response = await fetch('https://react-f984f-default-rtdb.firebaseio.com/cart.json', {
@@ -31,31 +33,50 @@ function App() {
         });
 
         if (response.ok) {
-          dispatch(uiActions.showNotification({
+            dispatch(uiActions.showNotification({
             status: "success",
             title: "Success",
             message: "Cart Data Sent Successfully"
-          }));
+          }));   
+
         }        
-        if (isInitial) {
-          isInitial = !isInitial;
-          return;
-        }
       } catch (err) {
-        dispatch(uiActions.showNotification({
+          dispatch(uiActions.showNotification({
           status: "error",
           title: "Error",
           message: "Cart Data not Sent Successfully"
-        }));
+        }));    
+
+      }finally{
+        setRequestInProgress(false);
       }
     }
 
-    sendCartData();
-  }, [cart, dispatch]);
+    //if (!isInitial) {
+
+      sendCartData();
+    // } else {
+    //   isInitial = false;
+    // }
+  }, [cart,dispatch]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://react-f984f-default-rtdb.firebaseio.com/cart.json');
+        const data = await response.json();
+        dispatch(cartActions.replaceCart(data));
+      } catch (error) {
+        console.error('Error fetching cart data:', error);
+      }
+    };
+
+    fetchData();
+  }, [dispatch]);
 
   return (
     <Fragment>
-      {notification && <Notification status={notification.status} title={notification.title} message={notification.message} />}
+      {requestInProgress && notification && <Notification status={notification.status} title={notification.title} message={notification.message} />}
       <Layout>
         {showCart && <Cart />}
         <Products />
